f43ff524e6fc0cfbc2b00a36cbdba0943bf3802e
liping123456 <627036123@qq.com>
2018-01-30 20:48:56 +0800

    新建四个子组件

--- ele/src/router/index.js
+++ ele/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+import Home from '../components/Home'
+import Find from '../components/Find'
+import Cart from '../components/Cart'
+import Mine from '../components/Mine'
+
+// 创建路由实例，并且配置路由规则
+export default new VueRouter({
+  // 这里写路由规则
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    // 这个path就是router-link里的to跳转的地址
+    // component就是根据路由地址，找到对应的组件，显示到router-view中
+    { path: '', component: Home },
+    { path: '/home', component: Home },
+    { path: '/find', component: Find },
+    { path: '/cart', component: Cart },
+    { path: '/mine', component: Mine }
   ]
 })
